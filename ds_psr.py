21fd454b93692ef1c254418555fb8904837a916d
Nina Gusinskaia <gusinskaia@lobster.cita.utoronto.ca>
2021-01-25 13:20:15 -0500

    add load_data and ds_psr initial files

---
+++ ds_psr.py
@@ -0,0 +1,349 @@
+import astropy.units as u
+import numpy as np
+import matplotlib.pyplot as plt
+from glob import glob
+import os, sys, math, time
+from scipy import interpolate
+from astropy.time import Time
+from astropy.visualization import quantity_support
+from astropy import constants as const
+import astropy
+from matplotlib.colors import LogNorm
+
+import matplotlib as mpl
+from scipy.sparse.linalg import eigsh
+from scipy.optimize import curve_fit
+import load_data as ld
+
+
+sys.path.insert(1, '/home/gusinskaia/scintools/scintools')
+import ththmod as THTH
+np.seterr(divide='ignore', invalid='ignore')
+
+
+def load_triple_spectrum(name='/mnt/scratch-lustre/gusinskaia/triple_system/5602579_AO_1400_ds.npz',factor=[1,1], tel='Undefined', psr='PSRJ0337+1715', pad_it=True, npad=3, mean0=True, wnoise=False):
+    ''' load  data from npz file into Spec object using load_data module
+    Takes:
+    filenpz - name of the npz file to load data from
+    factor - [#int, #int], where #int is always a factor of 2. 
+    factor difined the number by which to downsample 2D ds data (see shrink_2 function)
+    tel - Observatpry on which the data was taken (if known)
+    psr - pulsar name of the object (if known)
+    pad_it  - whether to pad the secondary spectra with zeros
+    npad - (int) number of paded zeros 
+    mean0 - set mean of the data to 0 (i.e. whether to subtract mean)
+    wnoise - whether to load noise data (which is present or not)
+    Returns:
+    Spec object'''
+
+    triple_ds=np.load(name)
+    ds,a_t,a_f, ns=ld.load_triple(name, factor=factor, mean0=mean0, wnoise=wnoise)
+    if tel == 'Undefined':
+        spt_tel=name.split('_')
+        if "AO" in spt_tel:
+            tel="AO"
+        if "WSRT" in spt_tel:
+            tel="WSRT"
+        if "GBT" in spt_tel:
+            tel="GBT"
+    if wnoise is False:
+        ns_info='no noise'
+    else:
+        ns_info='w noise'
+    my_spec=Spec(I=ds,t=a_t,f=a_f,stend=triple_ds['mjd'], nI=ns, tel=tel, psr=psr,pad_it=pad_it, npad=npad, ns_info=ns_info)
+    return my_spec
+
+
+##Function for making extent tuple for plotting
+def ext_find(x,y):
+    '''function from Daniels code'''
+    dx=np.diff(x).mean()
+    dy=np.diff(y).mean()
+    ext=[(x[0]-dx/2).value,(x[-1]+dx/2).value,(y[0]-dy/2).value,(y[-1]+dy/2).value]
+    return(ext)
+
+def fun_plot_ds_woaxis(ds, t, f, fig=plt.figure(figsize=(3,8), dpi=150), rect=[0.0,0.0,1.0,1.0]):
+    '''function that plots ds inside a given axis. The purpose is to avoid wasting time setting parameters right.
+    it takes:
+    ds - dynamic spectrum (2D array)
+    t, f - time and frequency axes (astropy quantity with units)
+    fig - figure to which to add an axis
+    rect - rectangular parameters of the axis'''
+
+    ax=fig.add_axes(rect)
+    vmin,vmax = np.percentile(ds,[1,99])
+    ax.imshow(ds.T,extent=(0,(t[-1].value-t[0].value)/3600.,np.amin(f).value,np.amax(f).value),
+           vmin=vmin, vmax=vmax, aspect='auto', origin='lower')
+    ax.set_xlabel('Time (hr)')
+    ax.set_ylabel('Frequency (%s)'%f.unit.to_string('latex'))
+
+def fun_plot_ds(ds, t, f, new_fig=True, figsize=(3,8), dpi=150, vmin=None, vmax=None):
+    '''function that plots ds. The purpose is to avoid wasting time setting parameters right.
+    it takes:
+    ds - dynamic spectrum (2D array)
+    t, f - time and frequency axes (astropy quantity with units)
+    newfig - whether or not one wishes/needs to create new plt.figure()
+    dpi - resolution of a newly created figure
+    vmin, vmax - value limits of ds for plt.imshow()'''
+
+    if new_fig is True:
+    	plt.figure(figsize=figsize, dpi=dpi)
+    if (vmin is None) and (vmax is None):
+        vmin,vmax = np.percentile(ds,[1,99])
+    plt.imshow(ds.T,extent=(0,(t[-1].value-t[0].value)/3600.,np.amin(f).value,np.amax(f).value),
+           vmin=vmin, vmax=vmax, aspect='auto', origin='lower')
+    plt.xlabel('Time (hr)')
+    plt.ylabel('Frequency (%s)'%f.unit.to_string('latex'))
+
+def fun_make_ss(ds, t, f, pad_it=True, npad=3):
+    '''Makes secondary spectrun from dynamic spectrum using Daniels funtions
+    Takes:
+    ds - dynamic spectrum (2D array)
+    t, f - time and frequency axes (astropy quantity with units)
+    pad_it  - whether to pad the secondary spectra with zeros
+    npad - (int) number of paded zeros
+    Returns:
+    Is - secodary specrum array
+    tau, fd  - delay and dopler shift
+    '''
+    if pad_it is True:
+        ds_pad=np.pad(ds.T,((0,npad*ds.T.shape[0]),(0,npad*ds.T.shape[1])),mode='constant',
+                        constant_values=ds.T.mean())
+        ds=ds_pad.T
+        fd=THTH.fft_axis(t,u.mHz,npad)
+        tau=THTH.fft_axis(f,u.us,npad)
+        Is=np.fft.fftshift(np.fft.fft2(ds.T))
+    else:
+        fd=THTH.fft_axis(t,u.mHz)
+        tau=THTH.fft_axis(f,u.us)
+        Is=np.fft.fftshift(np.fft.fft2(ds.T))    
+    return Is, tau, fd
+
+
+def fun_plot_ss(Is, tau, fd, fd_lim=[-1.5, 1.5], tau_lim=[0.0,1.4], vmin=None, vmax=None, new_fig=True, figsize=(3,2), dpi=150, cb=True):
+    ''' Plots secondary spectrum
+    Takes:
+    Is  - complex array- secodary specrum array
+    tau, df - delay and dopler shift
+    fd_lim, tau_lim - [float, float]  - limit of dopler shift and delay in plot
+    vmin, vmax - value limits of ds for plt.imshow()
+
+    new_fig - whether or not one wishes/needs to create new plt.figure()
+    figsize - (float, float) - size of the newly created figure
+    dpi - resolution of a newly created figure
+    cb - whether or not to show the colorbar
+    '''
+
+    SS_ext=ext_find(fd,tau)
+    if vmin is None:
+        vmin=np.median(np.abs(Is[(Is!=0)])**2)*2
+    if vmax is None:
+        vmax=np.max(np.abs(Is[(Is!=0)])**2)*np.exp(-2.7)/10
+    if tau_lim is None:
+        tau_lim=np.array([tau[0].value,tau[-1].value])
+    if fd_lim is None:
+        fd_lim=np.array([fd[0].value,fd[-1].value])
+    if new_fig is True:
+        fig=plt.figure(figsize=figsize, dpi=dpi, facecolor='w', edgecolor='k')
+    plt.imshow(np.abs(Is)**2,norm=LogNorm(vmax=vmax, vmin=vmin),aspect='auto',extent=SS_ext),
+    if cb is True:
+        plt.colorbar()
+
+    plt.xlabel(fd.unit.to_string('latex'))
+    plt.ylabel(tau.unit.to_string('latex'))
+    plt.xlim(fd_lim[0], fd_lim[1])
+    plt.ylim(tau_lim[0], tau_lim[1])
+
+ def find_nearest(array, value):
+    '''Finds index of the nearest to value number in an array
+    Takes: 
+    array - (np.array(dtype=float)) array with numbers
+    value - (float) - number to which the find the index of the closes number in an array
+    Returns:
+    idx (int)''' 
+    array = np.asarray(array)
+    idx = (np.abs(array - value)).argmin()
+    return idx
+
+#not sure this function is in use - could delete 
+def find_edges(I_cross, fd_cross, tau_cross, fd_lim, tau_lim):
+    fd_l=find_nearest(fd_cross, fd_lim[0])
+    fd_r=find_nearest(fd_cross, fd_lim[1])
+    tau_b=np.amax([find_nearest(tau_cross, tau_lim[0]),find_nearest(tau_cross, tau_lim[1])])-1
+    tau_t=np.amin([find_nearest(tau_cross, tau_lim[0]),find_nearest(tau_cross, tau_lim[1])])+1
+    return fd_l, fd_r, tau_b, tau_t
+
+
+def crop_array(my_array, idx=None, values=None):
+    if idx is not None and values is not None:
+        raise ValueError("Both selection chosen! Choose one: based either on index or value!")
+    elif values is not None:
+        if (values[0].unit != my_array.unit) or (values[0].unit != my_array.unit):
+            raise ValueError("Unit of an array and selection do not match!")
+        res_i0=find_nearest(my_array.value, values[0].value)
+        res_i1=find_nearest(my_array.value, values[1].value)
+        res_idx=np.sort(np.array([res_i0,res_i1]))
+    elif idx is not None:
+        res_idx=np.sort(np.array([idx[0],idx[1]]))
+    else:
+        res_idx=np.array([0,len(my_array)])
+    res_array=my_array[res_idx[0]:res_idx[1]+1]
+    res_idx=[res_idx[0], res_idx[1]+1]
+    return res_array, res_idx
+    
+def fun_select(ds,t,f, time_sel=None, freq_sel=None, freq_idx=None, time_idx=None, ns=None):
+    '''Crops array in time and frequency (given by values of time and/or frequency) or by indexes of the array
+    Takes:
+    ds - dynamic spectrum (2D array)
+    t, f -  time and frequency axes (astropy quantity with units)
+    time_sel, freq_sel - (astropy units quantites) [q,q] - min and max of time and frequency to cut the array with. 
+    time_idx, freq_idx - [int,int] - index edges in time and freq axes to crop array to
+    ns - noise spectrum (if present)
+    Returns:
+    I_sel, t_sel, f_sel - cropped ds, time and frequency
+    t_idx, f_idx - what?
+    nI_sel - cropped ns
+    '''
+    if (freq_sel is not None) or (freq_idx is not None):
+        f_sel, f_idx = crop_array(f, idx=freq_idx, values=freq_sel)
+    else:
+        f_idx=[0,len(f)]
+        f_sel=f[f_idx[0]:f_idx[1]]
+    if (time_sel is not None) or (time_idx is not None):
+        t_sel, t_idx = crop_array(t, idx=time_idx, values=time_sel)
+    else:
+        t_idx=[0,len(t)]
+        t_sel=t[t_idx[0]:t_idx[1]]
+    I_sel = ds[t_idx[0]:t_idx[1],f_idx[0]:f_idx[1]]
+    if ns is None:
+        nI_sel=np.random.normal(size=np.shape(I_sel))*np.std(I_sel)/6
+    else:
+        nI_sel=ns[t_idx[0]:t_idx[1],f_idx[0]:f_idx[1]]
+    return I_sel, t_sel, f_sel, t_idx, f_idx, nI_sel
+
+def fun_interp(ds, mjd, f, t_ed, f_ed, t_len, f_len, ns=None):
+     '''Interpolates ds to a new time and frequency axis
+    Takes:
+    ds - dynamic spectrum (2D array)
+    mjd - time axis in mjd (astropy time object)
+    f - frequency axis (astropy quantity with units)
+    t_ed - (q -astropy units quantites) [q,q] - edges of the desired time axis to be interpolated to
+    t_len - (int) - size of the desired time axis to be interpolated to 
+    f_ed - (q -astropy units quantites) [q,q] - edges of the desired frequency axis to be interpolated to
+    f_len - (int) - size of the desired frequency axis to be interpolated to 
+
+    ns - noise spectrum (if present)
+    Returns:
+    I_new - interpolated ds
+    t_sec_new - new time axis (q)
+    f_new - new frequency axis
+    t_int - new time axis (float)
+    nI_new - interpolated ns
+    '''
+    fun=interpolate.interp2d(f, mjd, ds, kind='linear')
+    new_ntbin=(t_ed[1].value-t_ed[0].value)/(t_len)
+    t_int = np.arange(t_ed[0].value,t_ed[1].value, new_ntbin)
+    new_nfbin=(f_ed[1].value-f_ed[0].value)/(f_len-1)
+    f_new = np.arange(f_ed[0].value,f_ed[1].value+new_nfbin, new_nfbin)
+    I_new = fun(f_new, t_int)
+    if ns is None:
+        nI_new=np.random.normal(size=np.shape(I_new))*np.std(I_new)/6
+    else:
+        nfun=interpolate.interp2d(f, mjd, ns, kind='linear')
+        nI_new=nfun(f_new,t_int)
+    t_sec_new = (np.arange(t_len) * new_ntbin * 24.*3600. *u.s)
+    return I_new, f_new, t_sec_new, t_int, nI_new
+
+
+
+##Curvature evolution with freuquency
+def eta_func(f0,A):
+    '''Daniels function'''
+    return(A/(f0**2))
+
+class SecSpec(object):
+    '''Seconday spectum object'''
+    def __init__(self, ds, Is, tau, fd):
+        self.ds = ds
+        self.Is = Is
+        self.tau = tau
+        self.fd = fd
+    def __repr__(self):
+        return "<Secondary spectrum>"
+
+
+class Spec(object):
+    '''Spectrum object'''
+    def __init__(self, I, t, f, stend=[0.0,1.0], nI=None, tel='Unknown', psr='PSRJ0337+1715',pad_it=True, npad=3, ns_info='no noise'):
+        self.I = I
+        self.t = t
+        self.f = f
+        self.nsinfo=ns_info
+        if nI is None:
+            self.nI=np.random.normal(size=np.shape(self.I))*np.std(self.I)/6
+            self.nsinfo='no noise'
+        else:
+            self.nI=nI
+        self.stend=stend
+        
+
+        full_time=(self.stend[1]-self.stend[0])
+        ntbin=full_time/len(self.t)
+        a_t_mjd = (np.arange(len(self.t)) * ntbin * u.d)+self.stend[0]*u.d
+        self.mjd=Time(a_t_mjd, format='mjd', scale='utc')
+                   
+        self.tel=tel
+        self.psr=psr
+        self.ss = self.make_ss(pad_it, npad)
+    def __repr__(self):
+        times=(self.stend[1]-self.stend[0])*24.
+        
+        mjd_c=(self.stend[1]+self.stend[0])/2.
+        return "<Dynamic spectrum: Dur: %.2f hr, Freq: %.2f - %2.f MHz, MJD: %.2f, PSR: %s, Tel: %s, %s>"%(times,np.amin(self.f).value, np.amax(self.f).value, mjd_c, self.psr, self.tel, self.nsinfo)
+    def plot_ds(self, new_fig=True, figsize=(3,8), dpi=150, vmin=None, vmax=None):
+        fun_plot_ds(self.I, self.t, self.f,new_fig=new_fig,figsize=figsize, dpi= dpi, vmin=vmin, vmax=vmax)
+    def plot_nds(self, new_fig=True, figsize=(3,8), dpi=150, vmin=None, vmax=None):
+        fun_plot_ds(self.nI, self.t, self.f,new_fig=new_fig,figsize=figsize, dpi= dpi, vmin=vmin, vmax=vmax)
+        
+    def make_ss(self, pad_it=True, npad=3):
+        Is, tau, fd=fun_make_ss(self.I, self.t, self.f, pad_it=pad_it, npad=npad)
+        return SecSpec(self, Is, tau, fd)   
+
+ 
+    def plot_ss(self,fd_lim=[-1.5, 1.5], tau_lim=[0.0,1.4], vmin=None, vmax=None, new_fig=True, figsize=(3,2), dpi=150, cb=True):
+        fun_plot_ss(self.ss.Is, self.ss.tau, self.ss.fd, fd_lim=fd_lim, tau_lim=tau_lim, vmin=vmin, vmax=vmax, new_fig=new_fig,
+                figsize=figsize, dpi=dpi, cb=cb)
+
+    def select(self, time_sel=None, freq_sel=None, freq_idx=None, time_idx=None, pad_it=True, npad=3):
+        if (time_sel is not None) and (time_sel[0].unit == u.s):
+            time_axis=self.t
+            I_sel, sec_sel, f_sel, t_idx, f_idx, nI_sel=fun_select(self.I,time_axis,self.f, time_sel=time_sel,
+                                                     freq_sel=freq_sel, freq_idx=freq_idx,time_idx=time_idx, ns=self.nI)
+            mjd_sel=self.mjd.mjd[t_idx[0]:t_idx[1]]*u.d
+        if (time_sel is not None) and (time_sel[0].unit == u.d):
+            time_axis=self.mjd.mjd * u.d
+            I_sel, mjd_sel, f_sel, t_idx, f_idx, nI_sel=fun_select(self.I,time_axis,self.f, time_sel=time_sel,
+                                                     freq_sel=freq_sel, freq_idx=freq_idx,time_idx=time_idx, ns=self.nI)
+            sec_sel=self.t[t_idx[0]:t_idx[1]]
+        
+        mjd_bin=mjd_sel[1]-mjd_sel[0]
+        I_sel = self.I[t_idx[0]:t_idx[1],f_idx[0]:f_idx[1]]
+        nI_sel = self.nI[t_idx[0]:t_idx[1],f_idx[0]:f_idx[1]]
+
+        return Spec(I=I_sel, t=sec_sel, f=f_sel, stend=[mjd_sel[0].value,mjd_sel[-1].value+mjd_bin.value], nI=nI_sel,tel=self.tel, psr=self.psr, pad_it=pad_it, npad=npad)
+
+    def interp(self, t_ed, f_ed, t_len, f_len, pad_it=True, npad=3):
+        I_new, f_new, t_sec_new, t_int, nI_new=fun_interp(self.I, self.mjd.mjd, self.f,
+                                                  t_ed=t_ed, f_ed=f_ed, t_len=t_len, f_len=f_len,
+                                                  pad_it=pad_it, npad=npad, ns=self.nI)
+
+        new_spec=Spec(I=I_new, t=t_sec_new, f=f_new*u.MHz, stend=[t_ed[0].value,t_ed[1].value], nI=nI_new,
+                        tel=self.tel, psr=self.psr, pad_it=pad_it, npad=npad)
+        return new_spec
+
+    def get_noise(self):
+        temp=np.fft.fftshift(np.abs(np.fft.fft2(self.I.T)/np.sqrt(self.I.T.shape[0]*self.I.T.shape[1]))**2)
+        N=np.sqrt(temp[:temp.shape[0]//6,:temp.shape[1]//6].mean())
+        return N
+
+
---
+++ load_data.py
@@ -0,0 +1,161 @@
+import astropy.units as u
+import numpy as np
+import matplotlib.pyplot as plt
+from glob import glob
+import os, sys, math, time
+from scipy import interpolate
+from astropy.time import Time
+from astropy.visualization import quantity_support
+
+from astropy import constants as const
+
+import astropy
+from matplotlib.colors import LogNorm
+import matplotlib as mpl
+
+def factor_of_two(x, n=5, odd=True):                                                                                                                                                    
+    '''finds closest smaller number with enough (defined by n) factors of 2 
+    takes: any number
+    returns: easy factorable number by 2'''
+    x=int(x)                                                                                                                                                                            
+    if (x%2!=0) and (odd is True):                                                                                                                                                      
+        x=x-1                                                                                                                                                                           
+    os.system("factor %d > numout.txt"%x)                                                                                                                                               
+    arr0=np.genfromtxt("numout.txt")                                                                                                                                                    
+    arr=arr0[1:len(arr0)]                                                                                                                                                               
+    twos=arr[(arr[:]==2)]                                                                                                                                                               
+    N=len(twos)                                                                                                                                                                         
+                                                                                                                                                                                        
+    while (N < n):
+        os.system("factor %d > numout.txt"%x)
+        arr0=np.genfromtxt("numout.txt")
+        arr=arr0[1:-1]
+        twos=arr[(arr[:]==2)]
+        N=len(twos)
+        x=x-2
+    print (arr)
+    return x
+
+
+def shrink_2(array, factor=[1,1], size=None):
+    '''downsamples 2D-data by a fixed factors of 2
+    takes: 2D-array (array) and factors by which to downsampe that array (factor) in each axis
+    size - the desired size in each axis (if known)
+    returns: downsampled 2D array'''
+    #ideally need error function for factor to always be factor of 2
+    if size is None:
+        size=[]
+        for j in range(0,len(factor)):
+            if factor[j]==1:
+                print (array.shape[j])
+                size.append(array.shape[j])
+            else:
+                size.append(factor_of_two(array.shape[j],n=int(math.log(factor[j],2)),odd=True))
+        size=np.array(size)
+        print (size)
+    else:
+        size=size
+
+    new_data=np.zeros((int(size[0]/factor[0]),array.shape[1]))
+    print (new_data.shape)
+    for i in range(0,size[0],factor[0]):
+        k=int(i/factor[0])-1
+        new_data[k,:]=np.mean(array[i:i+factor[0],:],axis=0)
+    array=new_data
+
+    nnew_data=np.zeros((array.shape[0],int(size[1]/factor[1])))
+    for i in range(0,size[1],factor[1]):
+        k=int(i/factor[1])
+        nnew_data[:,k]=np.mean(array[:,i:i+factor[1]],axis=1)
+    array=nnew_data
+
+    return nnew_data
+
+def shrink(array, factor=[1,1,1,1], size=None):
+    '''downsamples 4D-data by a fixed factors of 2
+    takes: 4D-array (array) and factors by which to downsampe that array (factor) in each axis
+    size - the desired size in each axis(if known)
+    returns: downsampled 4D array'''
+    #ideally need error function for factor to always be factor of 2
+    if size is None:
+        size=[]
+        for j in range(0,4):
+            if factor[j]==1:
+                size.append(array.shape[j])
+            else:
+                size.append(factor_of_two(array.shape[j],n=int(math.log(factor[j],2)),odd=True))
+        size=np.array(size)
+        print (size)
+    else:
+        size=size
+    
+    
+    new_data=np.zeros((int(size[0]/factor[0]),array.shape[1],array.shape[2],array.shape[3]))
+    print (new_data.shape)
+    for i in range(0,size[0]-factor[0],factor[0]):
+        k=int(i/factor[0])
+        new_data[k,:,:,:]=np.mean(array[i:i+factor[0],:,:,:],axis=0)
+    array=new_data
+    
+    nnew_data=np.zeros((array.shape[0],int(size[1]/factor[1]),array.shape[2],array.shape[3]))
+    for i in range(0,size[1],factor[1]):
+        k=int(i/factor[1])
+        nnew_data[:,k,:,:]=np.mean(array[:,i:i+factor[1],:,:],axis=1)
+    array=nnew_data
+    
+    nnnew_data=np.zeros((array.shape[0],array.shape[1],int(size[2]/factor[2]),array.shape[3]))
+    for i in range(0,size[2],factor[2]):
+        k=int(i/factor[2])
+        nnnew_data[:,:,k,:]=np.mean(array[:,:,i:i+factor[2],:],axis=2)
+    array=nnnew_data
+    
+    nnnnew_data=np.zeros((array.shape[0],array.shape[1],array.shape[2],int(size[3]/factor[3])))
+    for i in range(0,size[3],factor[3]):
+        k=int(i/factor[3])
+        nnnnew_data[:,:,:,k]=np.mean(array[:,:,:,i:i+factor[3]],axis=3)
+    return nnnnew_data
+
+
+def load_triple(filenpz='/mnt/scratch-lustre/gusinskaia/triple_system/5602579_AO_1400_ds.npz',factor=[1,1], mean0=True, wnoise=False):
+    '''loads ds data from npz file, downsamples it, subtructs mean (if applied) and loads noise data if present (and applied)
+    Takes:
+    filenpz - name of the npz file to load data from
+    factor - [#int, #int], where #int is always a factor of 2. 
+    factor difined the number by which to downsample 2D ds data (see shrink_2 function)
+    mean0 - set mean of the data to 0 (i.e. whether to subtract mean)
+    wnoise - whether to load noise data (which is present or not)
+    Returns:
+    ds - dynamic spectra (2D array)
+    t - time axis (astropy units quantity)
+    f - frequency axis (astropy units quantity)
+    ns - noise of dynamic spectra'''
+    triple_ds=np.load(filenpz)
+    print (triple_ds['ds'].shape)
+    ds=triple_ds['ds']
+    if wnoise is True:
+        ns=triple_ds['noise']
+    else:
+        ns=np.random.normal(size=np.shape(ds))*np.std(ds)/6
+    if 'WSRT' in filenpz:
+        ds=shrink_2(ds, factor=factor, size=None)
+        ns=shrink_2(ns, factor=factor, size=None)
+
+    if mean0 is True:
+        ds=ds-ds.mean()
+        ns=ns-ns.mean()
+    end_mjd=triple_ds['mjd'][1]
+    start_mjd=triple_ds['mjd'][0]
+    center_frequency=triple_ds['c_fr']
+    bw=triple_ds['bw_fr']
+
+    full_time=(end_mjd-start_mjd)*(24.*3600.)
+    ntbin=full_time/ds.shape[0]
+    a_t = (np.arange(ds.shape[0]) * ntbin * u.s)
+    a_t = (np.arange(ds.shape[0]) * ntbin * u.s)
+    if 'AO' in filenpz:
+        a_f = np.linspace(center_frequency+bw/2,center_frequency-bw/2, ds.shape[1]) * u.MHz
+        ds=np.flip(ds, axis=1)
+        ns=np.flip(ns, axis=1)
+    else:
+        a_f = np.linspace(center_frequency-bw/2,center_frequency+bw/2, ds.shape[1]) * u.MHz
+    return ds, a_t, a_f, ns
